70f41f744c1670f588e972faa1a9b8a06485781e
hellmir <hellmir610@gmail.com>
2025-01-27 11:04:30 +0900

    [LCSV-020] docs(swagger): Add swagger document for API and endpoint (#8)
    - app.py
    - routers.py
    - services.py

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from fastapi import FastAPI
 
 from routers import router
 
-app = FastAPI()
+app = FastAPI(
+    title="LLM 서버 API",
+    description="LLM 서비스를 제공하는 서버 API입니다.",
+    version="1.0",
+)
+
 app.include_router(router)
 
 if __name__ == "__main__":
